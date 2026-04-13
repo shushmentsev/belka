72859ca0507f1637a3419c39ac03f938cf4b64fa
Anton Shushmentsev <engineer7725@gmail.com>
2026-04-13 19:43:00 +0700

    Исправление бага с импортами

--- src/belka/rbac/__init__.py
+++ src/belka/rbac/__init__.py
@@ -1,10 +1,6 @@
-from belka.rbac.models import (
-    Permission,
-    PermissionRelationRole,
-    Role,
-    RoleRelationUser,
-    User,
-)
+from belka.rbac.permission.models import Permission, PermissionRelationRole
+from belka.rbac.role.models import Role, RoleRelationUser
+from belka.rbac.user.models import User
 
 __all__ = [
     "Permission",

7ea725ac7f0ba924065b7f12bf8f9a7bcbc3c37e
Anton Shushmentsev <engineer7725@gmail.com>
2026-04-14 15:05:12 +0700

    Переименование rbac в auth_rbac

---
+++ src/belka/auth_rbac/__init__.py
@@ -0,0 +1,11 @@
+from belka.auth_rbac.permission.models import Permission, PermissionRelationRole
+from belka.auth_rbac.role.models import Role, RoleRelationUser
+from belka.auth_rbac.user.models import User
+
+__all__ = [
+    "Permission",
+    "PermissionRelationRole",
+    "Role",
+    "RoleRelationUser",
+    "User",
+]
